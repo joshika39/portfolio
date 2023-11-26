2d5af91aceb384a35490314567fe32df16a176ca
Joshua Hegedus <josh.hegedus@outlook.com>
2023-11-26 20:04:32 +0100

    added base project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Joshua Hegedus</p>
+      <p>Software Engineer</p>
+    </div>
+  );
+}
+
+export default App;
