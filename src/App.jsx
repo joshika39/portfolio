78fe4eb04802a113578aa946cec8021c72c3551a
Joshua Hegedus <josh.hegedus@outlook.com>
2023-11-29 00:54:47 +0100

    fixed logo added tailwins

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-white dark:bg-black">
       <p>Joshua Hegedus</p>
       <p>Software Engineer</p>
     </div>
