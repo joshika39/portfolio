38f2c2b1fed3f32145b1a15a10b8a501a336e73b
Joshua Hegedus <josh.hegedus@outlook.com>
2025-06-15 21:42:58 +0200

    feat: added a layout, styles, images

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,16 @@
 import CleanCSS from "clean-css";
 
+/**
+ * @typedef {import("@11ty/eleventy").EleventyConfig} EleventyConfig
+ */
 export default function (eleventyConfig) {
-  eleventyConfig.addFilter("cssmin", function (code) {
-    return new CleanCSS({}).minify(code).styles;
-  });
+  eleventyConfig.setInputDirectory("src");
+
   eleventyConfig.addPassthroughCopy("src/assets/fonts");
   eleventyConfig.addPassthroughCopy("src/assets/images");
   eleventyConfig.addPassthroughCopy("src/assets/css");
+
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
 }
