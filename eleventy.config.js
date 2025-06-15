dc77765e8f223455bf7d55ffa666c30a1483b326
Joshua Hegedus <josh.hegedus@outlook.com>
2025-06-16 01:44:11 +0200

    feat: added icons and manifest.json

--- eleventy.config.js
+++ eleventy.config.js
@@ -8,9 +8,13 @@ export default function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/assets/fonts");
   eleventyConfig.addPassthroughCopy("src/assets/images");
+  eleventyConfig.addPassthroughCopy("src/assets/images/*.svg");
+  eleventyConfig.addPassthroughCopy("src/assets/images/*.ico");
   eleventyConfig.addPassthroughCopy("src/assets/css");
+  eleventyConfig.addPassthroughCopy("src/manifest.json");
 
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
+
 }
