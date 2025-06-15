f7a4f9d86a3b929b5701ca8a432a3b0e6d30fa6b
Joshua Hegedus <josh.hegedus@outlook.com>
2025-06-15 19:48:12 +0200

    feat: restarted with 11ty

---
+++ eleventy.config.js
@@ -0,0 +1,8 @@
+import CleanCSS from "clean-css";
+
+export default function (eleventyConfig) {
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+  eleventyConfig.addPassthroughCopy("font.woff2");
+}
