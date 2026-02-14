bd0c6bf58484088ce877cd22c5a864aca99db355
Joshua Hegedus <josh.hegedus@outlook.com>
2026-02-14 21:25:15 +0100

    added photo description

--- src/assets/js/photography.js
+++ src/assets/js/photography.js
@@ -6,41 +6,49 @@ const images = [
   {
     alt: "HYROX Team in a break.",
     name: "ahoy-captain",
+    description: "HYROX Team in a break.",
     extension: "jpg",
   },
   {
     alt: "Bilbao in sunshine.",
     name: "sunshine-bilbao",
+    description: "A sunny roundabout in Bilbao.",
     extension: "jpg",
   },
   {
     alt: "A cute orange cat.",
     name: "orange-cat",
+    description: "A cute orange cat.",
     extension: "jpg",
   },
   {
     alt: "A gradient background for wallpaper.",
     name: "wp-gradient-2",
+    description: "My wallpaper for my MacBook",
     extension: "jpg",
   },
   {
     alt: "A winter walk in Budapest.",
     name: "bp-winter-1",
+    description: "A wintery walk in Budapest.",
     extension: "jpg",
   },
   {
     alt: "Spring is coming.",
     name: "spring-is-coming",
+    description: "Spring is coming.",
     extension: "jpg",
   },
   {
     alt: "A rainbow on the concrete.",
     name: "concrete-rainbow",
+    description: "A rainbow on the concrete.",
     extension: "jpg",
   },
   {
     alt: "On the Margaret Bridge in Budapest.",
     name: "margaret-bridge-1",
+    description: "Goldest hour on the Margaret Bridge in Budapest.",
     extension: "jpg",
   }
 ];
@@ -66,6 +74,9 @@ const onImageClick = (image) => {
   modalImage.src = fullImageUrl;
   modalImage.alt = image.alt;
 
+  const modalDescription = document.createElement("p");
+  modalDescription.textContent = image.description;
+
   const closeButton = document.createElement("span");
   closeButton.innerHTML = "&times;";
   closeButton.classList.add("close-button");
@@ -75,6 +86,7 @@ const onImageClick = (image) => {
   });
 
   modal.appendChild(modalImage);
+  modal.appendChild(modalDescription);
   modal.appendChild(closeButton);
 
   modal.style.display = "flex";
@@ -87,8 +99,12 @@ for (let i = 0; i < images.length; i++) {
   imageElement.alt = image.alt;
   imageElement.addEventListener("click", () => onImageClick(image));
 
+  const descriptionElement = document.createElement("p");
+  descriptionElement.textContent = image.description;
+
   const linkElement = document.createElement("li");
   linkElement.appendChild(imageElement);
+  linkElement.appendChild(descriptionElement);
 
   imageList.appendChild(linkElement);
 }
