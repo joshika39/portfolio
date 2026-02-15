82544cd6a44fef10d1b4ebf14e395f740b54f69d
Joshua Hegedus <josh.hegedus@outlook.com>
2026-02-15 13:03:35 +0100

    added more images and wallpapers

---
+++ src/assets/js/images.js
@@ -0,0 +1,70 @@
+const images = [
+  {
+    alt: "HYROX Team in a break.",
+    name: "ahoy-captain",
+    description: "HYROX Team in a break.",
+    extension: "jpg",
+  },
+  {
+    alt: "Bilbao in sunshine.",
+    name: "sunshine-bilbao",
+    description: "A sunny roundabout in Bilbao.",
+    extension: "jpg",
+  },
+  {
+    alt: "A cute orange cat.",
+    name: "orange-cat",
+    description: "A cute orange cat.",
+    extension: "jpg",
+  },
+  {
+    alt: "A winter walk in Budapest.",
+    name: "bp-winter-1",
+    description: "A wintery walk in Budapest.",
+    extension: "jpg",
+  },
+  {
+    alt: "Spring is coming.",
+    name: "spring-is-coming",
+    description: "Spring is coming.",
+    extension: "jpg",
+  },
+  {
+    alt: "A rainbow on the concrete.",
+    name: "concrete-rainbow",
+    description: "A rainbow on the concrete.",
+    extension: "jpg",
+  },
+  {
+    alt: "On the Margaret Bridge in Budapest.",
+    name: "margaret-bridge-1",
+    description: "Goldest hour on the Margaret Bridge in Budapest.",
+    extension: "jpg",
+  },
+  {
+    alt: "Ducks in Danube near Szentendre.",
+    name: "ducks-danube",
+    description: "ducks.",
+    extension: "jpg",
+  },
+  {
+    alt: "A gradient background for wallpaper.",
+    name: "wp-gradient-2",
+    description: "My wallpaper for my MacBook",
+    extension: "jpg",
+  },
+  {
+    alt: "Another gradient background for wallpaper.",
+    name: "wp-gradient-3-light",
+    description: "Another gradient I use for wallpaper.",
+    extension: "jpg",
+  },
+  {
+    alt: "A darl gradient background for wallpaper.",
+    name: "wp-gradient-3-dark",
+    description: "My dark wallpaper for my MacBook",
+    extension: "jpg",
+  },
+];
+
+export default images;
--- src/assets/js/photography.js
+++ src/assets/js/photography.js
@@ -1,57 +1,6 @@
-console.log("Photography script loaded.");
+import images from "./images.js";
 
 const imagesPath = "/assets/images/portfolio";
-
-const images = [
-  {
-    alt: "HYROX Team in a break.",
-    name: "ahoy-captain",
-    description: "HYROX Team in a break.",
-    extension: "jpg",
-  },
-  {
-    alt: "Bilbao in sunshine.",
-    name: "sunshine-bilbao",
-    description: "A sunny roundabout in Bilbao.",
-    extension: "jpg",
-  },
-  {
-    alt: "A cute orange cat.",
-    name: "orange-cat",
-    description: "A cute orange cat.",
-    extension: "jpg",
-  },
-  {
-    alt: "A gradient background for wallpaper.",
-    name: "wp-gradient-2",
-    description: "My wallpaper for my MacBook",
-    extension: "jpg",
-  },
-  {
-    alt: "A winter walk in Budapest.",
-    name: "bp-winter-1",
-    description: "A wintery walk in Budapest.",
-    extension: "jpg",
-  },
-  {
-    alt: "Spring is coming.",
-    name: "spring-is-coming",
-    description: "Spring is coming.",
-    extension: "jpg",
-  },
-  {
-    alt: "A rainbow on the concrete.",
-    name: "concrete-rainbow",
-    description: "A rainbow on the concrete.",
-    extension: "jpg",
-  },
-  {
-    alt: "On the Margaret Bridge in Budapest.",
-    name: "margaret-bridge-1",
-    description: "Goldest hour on the Margaret Bridge in Budapest.",
-    extension: "jpg",
-  }
-];
 
 const imageList = document.querySelector("#photography-gallery");
 
